5b445e82e3a67d15a0659e2b3633a298e2586f4d
Matthew Boyette <Dyndrilliac@gmail.com>
2023-04-02 13:09:39 -0400

    Matthew: Changes requested by Rick just prior to go-live.

---
+++ Pages/AvailablePuppies.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace marleys_goldendoodles.Pages;
+
+public class AvailablePuppiesModel : PageModel
+{
+    private readonly ILogger<AvailablePuppiesModel> _logger;
+
+    public AvailablePuppiesModel(ILogger<AvailablePuppiesModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
